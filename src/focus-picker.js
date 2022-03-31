045a1a5144dc12243a6a9bbfad19071d54d708e2
Zaki Eleraky <zaki.eleraky@intelligencebank.com>
2022-03-31 16:36:51 +1100

    add auto focus

--- src/focus-picker.js
+++ src/focus-picker.js
@@ -1,27 +1,36 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import debounce from "lodash.debounce";
 import PropTypes from "prop-types";
 import { FocusPicker } from "image-focus";
 import smartcrop from "smartcrop";
 
-const startingFocus = { x: 0.1, y: 0.1 };
+const startingFocus = { x: -2, y: -2 };
 
 const FocusPickerComponent = ({
-  debounceTime = 200,
+  debounceTime = 300,
   imageSrc,
   onFocusChanged
 }) => {
   const imageRef = useRef(null);
 
+  const [focusCoordinates, setFocusCoordinates] = useState(startingFocus)
+
+
+  console.log(focusCoordinates);
+
   useEffect(() => {
     console.log("imgElRef.current", imageRef.current);
     let imageHTML = imageRef.current;
+
+    const originalWidth = imageHTML.clientWidth
+    const originalHeight = imageHTML.clientHeight
+
     console.log("html image", imageHTML);
 
     const asyncFn = async () => {
       const cropObj = await smartcrop.crop(imageHTML, {
-        width: 500,
-        height: 500,
+        width: originalHeight,
+        height: originalWidth,
         ruleOfThirds: true,
         minScale: 1.0
       });
@@ -33,6 +42,14 @@ const FocusPickerComponent = ({
       console.log(y);
       console.log(width);
       console.log(height);
+
+      const focusX = (x + width/2)/(originalWidth/2) - 1
+      const focusY = (y + height/2)/(originalHeight/2) - 1
+      console.log(focusX);
+      console.log(focusY);
+
+      setFocusCoordinates({ x: focusX, y: focusY })
+      
     };
     asyncFn();
   }, [imageSrc]);
@@ -42,12 +59,12 @@ const FocusPickerComponent = ({
       const debouncedOnFocusChanged = debounce(onFocusChanged, debounceTime);
       new FocusPicker(imageRef.current, {
         onChange: debouncedOnFocusChanged,
-        focus: startingFocus
+        focus: focusCoordinates
       });
     }
-  }, [debounceTime, onFocusChanged]);
+  }, [debounceTime, onFocusChanged, focusCoordinates]);
 
-  return <img alt="Focus picker" ref={imageRef} src={imageSrc} />;
+  return <img alt="Focus picker" ref={imageRef} src={imageSrc}/>;
 };
 
 FocusPickerComponent.defaultProps = {
--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
 import FocusedImage from "./focused-image";
 import FocusPicker from "./focus-picker";
 
 const App = () => {
   const startingFocus = { x: 0, y: 0 };
+  const [image, setImage] = React.useState(null);
   const [focus, setFocus] = useState(startingFocus);
-  const [imageUrl, setImageUrl] = useState("https://images.thewest.com.au/publication/C-6232124/b1e6744c8e69762e35141c3fc8c0051c6c25997f-16x9-x0y71w3000h1688.jpg?imwidth=1024&impolicy=wan_v3");
 
+console.log(image);
 
+  const fileHandler = files => {
+    let reader = new FileReader();
+    reader.onload = function() {
+      let image = reader.result;
+      setImage(null)
+      setImage(image);
+    };
+    reader.readAsDataURL(files[0]);
+  };
   return (
     <div className="app">
       <h1>Focused Image Picker</h1>
-      <div className="container">
+      <input
+        type="file"
+        onChange={e => {
+          fileHandler(e.target.files);
+        }}
+      /> 
+      <br/>
+      <br/>
+      {image && <div className="container">
         <div className="focus-picker-container">
           <FocusPicker
-            imageSrc={imageUrl}
+            imageSrc={image}
             onFocusChanged={setFocus}
           />
         </div>
@@ -26,70 +43,49 @@ const App = () => {
           <div className="grid">
             <div className="top-left">
               <FocusedImage
-                imageSrc={imageUrl}
+                imageSrc={image}
                 x={focus.x}
                 y={focus.y}
               />
             </div>
             <div className="top-center">
               <FocusedImage
-                imageSrc={imageUrl}
+                imageSrc={image}
                 x={focus.x}
                 y={focus.y}
               />
             </div>
             <div className="top-right">
               <FocusedImage
-                imageSrc={imageUrl}
+                imageSrc={image}
                 x={focus.x}
                 y={focus.y}
               />
             </div>
             <div className="center-left">
               <FocusedImage
-                imageSrc={imageUrl}
+                imageSrc={image}
                 x={focus.x}
                 y={focus.y}
               />
             </div>
             <div className="center-center">
               <FocusedImage
-                imageSrc={imageUrl}
+                imageSrc={image}
                 x={focus.x}
                 y={focus.y}
               />
             </div>
             <div className="center-right">
               <FocusedImage
-                imageSrc={imageUrl}
-                x={focus.x}
-                y={focus.y}
-              />
-            </div>
-            <div className="bottom-left">
-              <FocusedImage
-                imageSrc={imageUrl}
-                x={focus.x}
-                y={focus.y}
-              />
-            </div>
-            <div className="bottom-center">
-              <FocusedImage
-                imageSrc={imageUrl}
-                x={focus.x}
-                y={focus.y}
-              />
-            </div>
-            <div className="bottom-right">
-              <FocusedImage
-                imageSrc={imageUrl}
+                imageSrc={image}
                 x={focus.x}
                 y={focus.y}
               />
             </div>
           </div>
         </div>
-      </div>
+      </div>}
     </div>
   );
 };
