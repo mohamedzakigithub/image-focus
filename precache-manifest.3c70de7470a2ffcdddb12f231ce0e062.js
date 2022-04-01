4c2c1d5d6e8cc3fee45e93b265dd2f6dbf5f10ec
Zaki Eleraky <zaki.eleraky@intelligencebank.com>
2022-04-01 13:42:18 +1100

    Updates

---
+++ precache-manifest.3c70de7470a2ffcdddb12f231ce0e062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615302de1c1c7a2525385a0e78770a99",
+    "url": "/image-focus/index.html"
+  },
+  {
+    "revision": "937290915c67fecc91c4",
+    "url": "/image-focus/static/css/main.a031bbda.chunk.css"
+  },
+  {
+    "revision": "b33925f7c44e2faf915e",
+    "url": "/image-focus/static/js/2.3030629f.chunk.js"
+  },
+  {
+    "revision": "937290915c67fecc91c4",
+    "url": "/image-focus/static/js/main.ef324384.chunk.js"
+  },
+  {
+    "revision": "082137ea356229983179",
+    "url": "/image-focus/static/js/runtime~main.93e6f085.js"
+  }
+]);
