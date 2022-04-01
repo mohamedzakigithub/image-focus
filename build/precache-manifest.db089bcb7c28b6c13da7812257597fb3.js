e9e1ccaeeea4566b594e1d6feb18fe49841f18f8
Zaki Eleraky <zaki.eleraky@intelligencebank.com>
2022-04-01 13:36:47 +1100

    error fix

---
+++ build/precache-manifest.db089bcb7c28b6c13da7812257597fb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d4c95891dfc4c8ff1d7419f9375849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "937290915c67fecc91c4",
+    "url": "/static/css/main.a031bbda.chunk.css"
+  },
+  {
+    "revision": "e063ed93d538926d6a90",
+    "url": "/static/js/2.2fe71103.chunk.js"
+  },
+  {
+    "revision": "937290915c67fecc91c4",
+    "url": "/static/js/main.ef324384.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
